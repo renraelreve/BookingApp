bd4eecbe67c8bba97688dff3486fbea911cd8062
recharging <recharging@users.noreply.github.com>
2024-08-22 12:51:23 +0800

    link to heroku Backend

--- api/bookingApi.js
+++ api/bookingApi.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.1.11:8080/"
-// const BASE_URL = "https://booking-api-55ab36857552.herokuapp.com/";
+const BASE_URL = "https://booking-api-55ab36857552.herokuapp.com/";
 
 export const bookingApi = axios.create({ baseURL: BASE_URL });
